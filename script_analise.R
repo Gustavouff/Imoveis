0709a8148467d0a1b29385c961df388b9b8639e8
Gustavo Oliva <gustavooliva@id.uff.br>
2026-04-01 00:07:36 -0300

    Adicionando graficos de histograma e dispersao

--- script_analise.R
+++ script_analise.R
@@ -1,2 +1,13 @@
 dados <- read.csv("imoveis.csv", sep = ";")
 summary(dados)
+hist(dados$preco,
+     main = "Histograma de Preços",
+     xlab = "Preço",
+     ylab = "Frequência",
+     col = "lightblue")
+
+plot(x = dados$metragem, y = dados$imposto_anual,
+     main = "Dispersão: Imposto vs Metragem",
+     xlab = "Metragem",
+     ylab = "Imposto Anual",
+     pch = 19, col = "darkred")
